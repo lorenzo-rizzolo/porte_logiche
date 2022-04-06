c3533c840ad3b84af9f1b797ee5820c13f0d1b1c
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-04-06 19:33:25 +0200

    ,kgjy

---
+++ src/pages/home.svelte
@@ -0,0 +1,58 @@
+<Page name="home">
+  <div class="bar">
+    <Navbar>
+      <NavTitle>PORTE LOGICHE</NavTitle>
+    </Navbar>
+  </div>
+
+    <List accordionList>
+            <ListItem accordionItem title="7408 - AND"> 
+                <AccordionContent>
+                    <Block>
+                        
+                    </Block>
+                </AccordionContent>
+            </ListItem> 
+        <ListItem accordionItem title="7404 - NOT"> 
+            <AccordionContent>
+                <Block>
+                    
+                </Block>
+            </AccordionContent>
+        </ListItem> 
+        <ListItem accordionItem title="7405 - NAND"> 
+          <AccordionContent>
+              <Block>
+                  
+              </Block>
+          </AccordionContent>
+        </ListItem> 
+    </List>
+</Page>
+<script>
+  import {
+    Page,
+    Accordion,
+    AccordionContent,
+
+    Navbar,
+    NavLeft,
+    NavTitle,
+    NavTitleLarge,
+    NavRight,
+    Link,
+    Toolbar,
+    Block,
+    BlockTitle,
+    List,
+    ListItem,
+    Row,
+    Col,
+    Button
+  } from 'framework7-svelte';
+</script>
+<style>
+  .bar{
+    background-color: blue;
+  }
+</style>
